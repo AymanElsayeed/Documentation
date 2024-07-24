0f5c838c8d3930c9068253c8dabb26b88f225f53
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-07-24 19:04:31 +0300

    init commit.

---
+++ example/example_module.py
@@ -0,0 +1,15 @@
+"""
+
+This is an example module.
+
+
+"""
+
+
+def example_function():
+    """
+    This is an example function
+    :return: None
+    """
+    print("example_function")
+    return None
---
+++ example/example_module_2.py
@@ -0,0 +1,20 @@
+"""
+
+This is an example 2 module.
+
+
+"""
+
+
+def example_function_2(param_one: int, param_two: int):
+    """
+    :param param_one: first parameter
+    :type param_one: int
+    :param param_two: second parameter
+    :type param_two: int
+    This is an example function
+    :return: sum of two parameters
+    :rtype: int
+    """
+    print("example_function")
+    return param_one + param_two
